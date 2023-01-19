c329dfb91bc44a504f0111b9c55f32473ebc1529
kayode <adeniyikayode22@gmail.com>
2023-01-19 17:53:17 +0100

    component to sort array

---
+++ drag-drop/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+import draggable from 'vuedraggable';
+const meals = ref([
+  'Hamburger',
+  'Pizza',
+  'Spaghetti',
+  'Tacos',
+  'Teriyaki Chicken',
+]);
+</script>
+
+<template>
+  <h1>Favorite Foods</h1>
+  <draggable v-model="meals" tag="ul">
+    <template #item="{ element: meal }">
+      <li>{{ meal }}</li>
+    </template>
+  </draggable>
+</template>
+
+
+
+
